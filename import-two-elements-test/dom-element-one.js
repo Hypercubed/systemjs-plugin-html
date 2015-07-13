5dc55cea58410ecee9b238bb4501e7c998ceb3c7
J. Harshbarger <hypercubed@gmail.com>
2015-07-13 15:52:18 +0900

    Renamed examples

--- import-two-elements-test/dom-element-one.js
+++
@@ -1,5 +0,0 @@
-import './dom-element-one.html!';
-
-export var domElementOne = Polymer({
-  is: 'dom-element-one'
-});
--- import-two-elements-test/dom-element-two.js
+++
@@ -1,5 +0,0 @@
-import './dom-element-two.html!';
-
-export var domElementTwo = Polymer({
-  is: 'dom-element-two'
-});
