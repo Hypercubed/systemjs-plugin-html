814691415350cf47d1da8181f45c31c6dec1cad1
Herculano Campos <herkulano@gmail.com>
2015-07-15 12:04:45 +0100

    Prepare for PR

---
+++ test/import-two-elements/dom-element-one.js
@@ -0,0 +1,8 @@
+import doc from './dom-element-one.html!';
+import Polymer from 'polymerjs';
+
+var proto = Polymer({
+  is: 'dom-element-one'
+});
+
+export { proto as prototype, doc as document };
