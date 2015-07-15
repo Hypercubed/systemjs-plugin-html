e36365a4b18fbaba1683283f95d84cc3ec2f9283
Herculano Campos <herkulano@gmail.com>
2015-07-15 11:35:57 +0100

    Removed files to prepare for merge

--- dom-element-one.js
+++
@@ -1,9 +0,0 @@
-import Polymer from 'greenify/polymer-js';
-
-import 'dom-element-two';
-
-import './dom-element-one.html!';
-
-export var domElementOne = Polymer({
-  is: 'dom-element-one'
-});
--- dom-element-two.js
+++
@@ -1,6 +0,0 @@
-import Polymer from 'greenify/polymer-js';
-import './dom-element-two.html!';
-
-export var domElementTwo = Polymer({
-  is: 'dom-element-two'
-});
