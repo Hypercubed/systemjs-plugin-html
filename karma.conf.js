9f7e16400010921417be87715caf87c11b5a6a31
J. Harshbarger <hypercubed@gmail.com>
2015-07-17 17:44:56 +0900

    Install jspm dev deps

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
   };
 
   if(process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci','Firefox'];
+    configuration.browsers = ['Firefox','Chrome_travis_ci'];
   }
 
   config.set(configuration);
